7007f9ca9ceb4c58b9fb8cf1572e92b5be824ecc
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-15 21:20:28 +0800

    initial

---
+++ backend/WebApi/Controllers/V1/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers.V1
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class ImageController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Image> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Image
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserId = Guid.NewGuid().ToString(),
+                Path = "/path",
+                CreatedAt = DateTimeOffset.UtcNow,
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ backend/WebApi/Models/Image.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.Models
+{
+    public class Image
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Path { get; set; }
+
+        public DateTimeOffset CreatedAt { get; set; }
+    }
+}
