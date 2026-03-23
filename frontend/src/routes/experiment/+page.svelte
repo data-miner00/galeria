7276d4c2bdcdb34d9108769c64da6061e89246ea
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-23 19:10:09 +0800

    chore: Add experiment page

---
+++ frontend/src/routes/experiment/+page.svelte
@@ -0,0 +1,47 @@
+<!-- Reference: https://flowbite.com/docs/components/gallery/ -->
+<div class="grid gap-4">
+	<div>
+		<img
+			class="rounded-base h-auto max-w-full"
+			src="https://flowbite.s3.amazonaws.com/docs/gallery/featured/image.jpg"
+			alt=""
+		/>
+	</div>
+	<div class="grid grid-cols-5 gap-4">
+		<div>
+			<img
+				class="rounded-base h-auto max-w-full"
+				src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg"
+				alt=""
+			/>
+		</div>
+		<div>
+			<img
+				class="rounded-base h-auto max-w-full"
+				src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg"
+				alt=""
+			/>
+		</div>
+		<div>
+			<img
+				class="rounded-base h-auto max-w-full"
+				src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg"
+				alt=""
+			/>
+		</div>
+		<div>
+			<img
+				class="rounded-base h-auto max-w-full"
+				src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg"
+				alt=""
+			/>
+		</div>
+		<div>
+			<img
+				class="rounded-base h-auto max-w-full"
+				src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg"
+				alt=""
+			/>
+		</div>
+	</div>
+</div>
