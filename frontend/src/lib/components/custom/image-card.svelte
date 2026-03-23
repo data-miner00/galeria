00d3e14695192f355b61b66547b9d631f019ff60
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-23 12:00:23 +0800

    wip: Basic homepage

---
+++ frontend/src/lib/components/custom/image-card.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Ellipsis from '@lucide/svelte/icons/ellipsis';
+	import Button from '../ui/button/button.svelte';
+
+	type Props = {
+		id: string;
+		path: string;
+		description?: string;
+	};
+
+	const { id, path, description }: Props = $props();
+</script>
+
+<div>
+	<div class="mb-1 w-70 overflow-hidden rounded">
+		<img alt={description} src={`http://127.0.0.1:10003/devstoreaccount1/images/${path}`} />
+	</div>
+	<div class="flex items-center justify-between">
+		<div>
+			<p class="max-w-40 text-sm text-ellipsis">{description}</p>
+		</div>
+		<Button variant="ghost" size="icon">
+			<Ellipsis />
+		</Button>
+	</div>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import ImageCard from '$lib/components/custom/image-card.svelte';
+	import { onMount } from 'svelte';
+
+	type ImageRecord = {
+		id: string;
+		path: string;
+		originalFileName: string;
+		contentType: string;
+		description?: string;
+		status: number;
+		createdAt: string;
+	};
+
+	let records = $state<ImageRecord[]>([]);
+
+	onMount(async () => {
+		const res = await fetch('https://localhost:7146/api/v1/image');
+		records = await res.json();
+	});
+</script>
+
+<div class="grid h-full grid-cols-5 gap-4">
+	<div class="flex w-70 flex-col gap-4">
+		{#each records as record}
+			<ImageCard id={record.id} path={record.path} description={record.description} />
+		{/each}
+	</div>
+	<div class="flex w-70 flex-col gap-4">
+		{#each records as record}
+			<ImageCard id={record.id} path={record.path} description={record.description} />
+		{/each}
+	</div>
+	<div class="flex w-70 flex-col gap-4">
+		{#each records as record}
+			<ImageCard id={record.id} path={record.path} description={record.description} />
+		{/each}
+	</div>
+	<div class="flex w-70 flex-col gap-4">
+		{#each records as record}
+			<ImageCard id={record.id} path={record.path} description={record.description} />
+		{/each}
+	</div>
+	<div class="flex w-70 flex-col gap-4">
+		{#each records as record}
+			<ImageCard id={record.id} path={record.path} description={record.description} />
+		{/each}
+	</div>
+</div>
