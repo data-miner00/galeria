4b5949a9ddbc518460d61cdb3a7b6bfd9f89a4d7
Shaun Chong <data-miner00@users.noreply.github.com>
2026-03-23 11:58:49 +0800

    feat: Improve header

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -3,9 +3,11 @@
 	import * as Breadcrumb from '$lib/components/ui/breadcrumb/index.js';
 	import { Separator } from '$lib/components/ui/separator/index.js';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { ArrowLeft, SearchIcon } from '@lucide/svelte';
 
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { Button } from '$lib/components/ui/button';
 
 	let { children } = $props();
 </script>
@@ -17,22 +19,33 @@
 
 <Sidebar.Provider>
 	<AppSidebar />
-	<Sidebar.Inset>
-		<header class="flex h-16 shrink-0 items-center gap-2">
-			<div class="flex items-center gap-2 px-4">
-				<Sidebar.Trigger class="-ms-1" />
-				<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
-				<Breadcrumb.Root>
-					<Breadcrumb.List>
-						<Breadcrumb.Item class="hidden md:block">
-							<Breadcrumb.Link href="##">Build Your Application</Breadcrumb.Link>
-						</Breadcrumb.Item>
-						<Breadcrumb.Separator class="hidden md:block" />
-						<Breadcrumb.Item>
-							<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-						</Breadcrumb.Item>
-					</Breadcrumb.List>
-				</Breadcrumb.Root>
+	<Sidebar.Inset class="relative overflow-clip">
+		<header class="sticky top-0 right-0 left-0 flex h-16 shrink-0 items-center gap-2 bg-white">
+			<div class="flex w-full items-center justify-between px-4">
+				<div class="flex items-center gap-2">
+					<Sidebar.Trigger class="-ms-1" />
+					<Separator orientation="vertical" class="data-[orientation=vertical]:h-4" />
+					<Button variant="ghost" size="icon">
+						<ArrowLeft />
+					</Button>
+					<Separator orientation="vertical" class="me-2 data-[orientation=vertical]:h-4" />
+					<Breadcrumb.Root>
+						<Breadcrumb.List>
+							<!-- <Breadcrumb.Item class="hidden md:block">
+								<Breadcrumb.Link href="##">At The Homepage</Breadcrumb.Link>
+							</Breadcrumb.Item>
+							<Breadcrumb.Separator class="hidden md:block" /> -->
+							<Breadcrumb.Item>
+								<Breadcrumb.Page>Home</Breadcrumb.Page>
+							</Breadcrumb.Item>
+						</Breadcrumb.List>
+					</Breadcrumb.Root>
+				</div>
+				<div>
+					<Button variant="ghost" size="icon">
+						<SearchIcon />
+					</Button>
+				</div>
 			</div>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
