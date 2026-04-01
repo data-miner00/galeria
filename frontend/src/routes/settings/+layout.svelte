abea55e194ecb7d90991a7d9daf688141a89a72c
Shaun Chong <data-miner00@users.noreply.github.com>
2026-04-01 23:38:50 +0800

    feat: Improved the side navigation for settings

--- frontend/src/routes/settings/+layout.svelte
+++ frontend/src/routes/settings/+layout.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
 	let { children } = $props();
+	import { page } from '$app/state';
 </script>
 
-<div class="mt-10 flex">
-	<aside class="flex basis-72 flex-col gap-5 text-lg">
-		<a href="/settings/profile" class="underline">Profile</a>
-		<a href="/settings/layout">Layout</a>
-		<a href="/settings/boards">Boards</a>
-		<a href="/settings/privacy">Privacy</a>
+<div class="mt-10 flex px-4">
+	<aside class="flex basis-72 flex-col gap-5">
+		<a href="/settings/general" class:underline={page.url.pathname === '/settings/general'}
+			>Application Generals</a
+		>
+		<a href="/settings/profile" class:underline={page.url.pathname === '/settings/profile'}
+			>Profile Related</a
+		>
+		<a href="/settings/layout" class:underline={page.url.pathname === '/settings/layout'}
+			>Layout Management</a
+		>
+		<a href="/settings/boards" class:underline={page.url.pathname === '/settings/boards'}
+			>Boards Settings</a
+		>
+		<a href="/settings/privacy" class:underline={page.url.pathname === '/settings/privacy'}
+			>Privacy Policy</a
+		>
 	</aside>
 
 	<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
