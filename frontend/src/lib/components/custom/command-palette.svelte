1b40cdef5761b61f8fb25e84ba75b6e348cc629e
Shaun Chong <data-miner00@users.noreply.github.com>
2026-07-05 16:49:41 +0800

    feat: Add more shortcuts

--- frontend/src/lib/components/custom/command-palette.svelte
+++ frontend/src/lib/components/custom/command-palette.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
-	import { FolderIcon, ImageIcon } from '@lucide/svelte';
+	import {
+		BanIcon,
+		FolderIcon,
+		HouseIcon,
+		ImageIcon,
+		RecycleIcon,
+		StarIcon,
+		TimelineIcon,
+		Undo2Icon
+	} from '@lucide/svelte';
 	import SettingsIcon from '@lucide/svelte/icons/settings';
 	import UserIcon from '@lucide/svelte/icons/user';
 
@@ -27,6 +36,11 @@
 		appState.openState.isCreateBoardDialogOpen = true;
 		isOpen = false;
 	}
+
+	function goBack() {
+		history.back();
+		isOpen = false;
+	}
 </script>
 
 <Command.Dialog class="rounded-lg border shadow-md md:min-w-112.5" bind:open={isOpen}>
@@ -42,7 +56,39 @@
 				<FolderIcon />
 				<span>Create Board</span>
 			</Command.Item>
+			<Command.Item onSelect={goBack}>
+				<Undo2Icon />
+				<span>Go Back</span>
+			</Command.Item>
 		</Command.Group>
+		<Command.Separator />
+		<Command.Group heading="Pages">
+			<Command.Item onSelect={() => gotoPage('/')}>
+				<HouseIcon />
+				<span>Home</span>
+			</Command.Item>
+			<Command.Item onSelect={() => gotoPage('/boards')}>
+				<FolderIcon />
+				<span>Boards</span>
+			</Command.Item>
+			<Command.Item onSelect={() => gotoPage('/recycle')}>
+				<RecycleIcon />
+				<span>Recycle Bin</span>
+			</Command.Item>
+			<Command.Item onSelect={() => gotoPage('/timeline')}>
+				<TimelineIcon />
+				<span>Timeline</span>
+			</Command.Item>
+			<Command.Item onSelect={() => gotoPage('/favorites')}>
+				<StarIcon />
+				<span>Favorites</span>
+			</Command.Item>
+			<Command.Item onSelect={() => gotoPage('/hidden')}>
+				<BanIcon />
+				<span>Hidden</span>
+			</Command.Item>
+		</Command.Group>
+
 		<Command.Separator />
 		<Command.Group heading="Settings">
 			<Command.Item onSelect={() => gotoPage('/settings/profile')}>
