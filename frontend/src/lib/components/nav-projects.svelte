00c7f962af09095ddd78e0a4ec6ff195449fb1b0
Shaun Chong <data-miner00@users.noreply.github.com>
2026-07-05 13:33:57 +0800

    refactor: Use all boards to go to listing page

--- frontend/src/lib/components/nav-projects.svelte
+++ frontend/src/lib/components/nav-projects.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
-	import type { Board } from '$lib/types';
 	import { FrameIcon, PinIcon, PinOffIcon } from '@lucide/svelte';
 	import EllipsisIcon from '@lucide/svelte/icons/ellipsis';
 	import FolderIcon from '@lucide/svelte/icons/folder';
 	import ShareIcon from '@lucide/svelte/icons/share';
 	import Trash2Icon from '@lucide/svelte/icons/trash-2';
 	import { toast } from 'svelte-sonner';
-	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
-	import { appState } from '$lib/states.svelte';
+
 	import { PUBLIC_API_BASE_URL } from '$env/static/public';
-	import { goto } from '$app/navigation';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog/index.js';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import { useSidebar } from '$lib/components/ui/sidebar/index.js';
+	import { appState } from '$lib/states.svelte';
+	import type { Board } from '$lib/types';
 
 	let {
 		boards
@@ -67,6 +67,14 @@
 <Sidebar.Group class="group-data-[collapsible=icon]:hidden">
 	<Sidebar.GroupLabel>Boards</Sidebar.GroupLabel>
 	<Sidebar.Menu>
+		<Sidebar.MenuItem>
+			<Sidebar.MenuButton>
+				<a href="/boards" class="flex w-full items-center gap-2">
+					<FolderIcon />
+					<span>All Boards</span>
+				</a>
+			</Sidebar.MenuButton>
+		</Sidebar.MenuItem>
 		{#each boards as item (item.id)}
 			<Sidebar.MenuItem>
 				<Sidebar.MenuButton>
@@ -121,12 +129,6 @@
 				</DropdownMenu.Root>
 			</Sidebar.MenuItem>
 		{/each}
-		<Sidebar.MenuItem>
-			<Sidebar.MenuButton onclick={() => goto('/boards')}>
-				<EllipsisIcon />
-				<span>More</span>
-			</Sidebar.MenuButton>
-		</Sidebar.MenuItem>
 	</Sidebar.Menu>
 </Sidebar.Group>
 
